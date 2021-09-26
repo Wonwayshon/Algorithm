121e088bd3c6d3dd28a8a7c488773d1d596822b7
Isaac Wonwayshon <1287690071@qq.com>
2021-09-26 11:48:12 +0800

    红黑树邻接表实现

---
+++ GraphBasics/src/AdjList.java
@@ -0,0 +1,97 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class AdjList {
+    private int V;
+    private int E;
+    private LinkedList<Integer>[] adj;
+
+    public AdjList(String filename) {
+        File file = new File(filename);
+        try (Scanner scanner = new Scanner(file)) {
+            V = scanner.nextInt();
+            if(V<0){
+                throw new IllegalArgumentException("Error Negative V");
+            }
+
+            adj = new LinkedList[V];
+            for (int i = 0; i < V; i++) {
+                adj[i] = new LinkedList<>();
+            }
+
+            E = scanner.nextInt();
+            if(E<0){
+                throw new IllegalArgumentException("Error Negative V");
+            }
+            for(int i=0;i<E;i++){
+                int a = scanner.nextInt();
+                validateVertex(a);
+                int b = scanner.nextInt();
+                validateVertex(b);
+
+                if(a==b){
+                    throw new IllegalArgumentException("Self Loop Not Supported!");
+                }
+                if(adj[a].contains(b)){
+                    throw new IllegalArgumentException("Parallel Edges Not Supported!");
+                }
+
+                adj[a].add(b);
+                adj[b].add(a);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void validateVertex(int v){
+        if(v<0 || v>=V){
+            throw new IllegalArgumentException("Illegal Argument v = "+v);
+        }
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public boolean hasEdge(int v,int w){
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v].contains(w);
+    }
+
+    public LinkedList<Integer> adj(int v){
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int degree(int v){
+        return adj(v).size();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("V=%d, E=%d\n", V, E));
+        for(int i=0;i<V;i++){
+            sb.append(String.format("%d: ",i));
+            for(int w:adj[i]){
+                sb.append(String.format("%d ", w));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        AdjList adjList = new AdjList("GraphBasics/g.txt");
+        System.out.println(adjList);
+    }
+}
---
+++ GraphBasics/src/AdjSet.java
@@ -0,0 +1,98 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class AdjSet {
+    private int V;
+    private int E;
+    private TreeSet<Integer>[] adj;
+
+    public AdjSet(String filename) {
+        File file = new File(filename);
+        try (Scanner scanner = new Scanner(file)) {
+            V = scanner.nextInt();
+            if(V<0){
+                throw new IllegalArgumentException("Error Negative V");
+            }
+
+            adj = new TreeSet[V];
+            for (int i = 0; i < V; i++) {
+                adj[i] = new TreeSet<>();
+            }
+
+            E = scanner.nextInt();
+            if(E<0){
+                throw new IllegalArgumentException("Error Negative V");
+            }
+            for(int i=0;i<E;i++){
+                int a = scanner.nextInt();
+                validateVertex(a);
+                int b = scanner.nextInt();
+                validateVertex(b);
+
+                if(a==b){
+                    throw new IllegalArgumentException("Self Loop Not Supported!");
+                }
+                if(adj[a].contains(b)){
+                    throw new IllegalArgumentException("Parallel Edges Not Supported!");
+                }
+
+                adj[a].add(b);
+                adj[b].add(a);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void validateVertex(int v){
+        if(v<0 || v>=V){
+            throw new IllegalArgumentException("Illegal Argument v = "+v);
+        }
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public boolean hasEdge(int v,int w){
+        validateVertex(v);
+        validateVertex(w);
+        return adj[v].contains(w);
+    }
+
+    public Iterable<Integer> adj(int v){
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int degree(int v){
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("V=%d, E=%d\n", V, E));
+        for(int i=0;i<V;i++){
+            sb.append(String.format("%d: ",i));
+            for(int w:adj[i]){
+                sb.append(String.format("%d ", w));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        AdjSet adjSet = new AdjSet("GraphBasics/g.txt");
+        System.out.println(adjSet);
+    }
+}
