866458e0a3d49fc662cdfb528755f7beffb7a741
Isaac Wonwayshon <1287690071@qq.com>
2021-10-10 10:48:28 +0800

    图的深度优先先序遍历和后序遍历

--- GraphDFS/src/GraphDFS.java
+++ GraphDFS/src/GraphDFS.java
@@ -1,34 +1,46 @@
 import java.util.ArrayList;
 
 public class GraphDFS {
-    private ArrayList<Integer> order = new ArrayList<>();
+    private ArrayList<Integer> pre = new ArrayList<>();
+    private ArrayList<Integer> post = new ArrayList<>();
     private Graph graph;
     private boolean[] visited;
 
     public GraphDFS(Graph graph){
         this.graph = graph;
         visited = new boolean[graph.V()];
-        dfs(0);
+        for (int v = 0; v < graph.V(); v++) {
+            if(!visited[v]){
+                dfs(v);
+            }
+        }
     }
 
     private void dfs(int v){
         visited[v] = true;
-        order.add(v);
+        pre.add(v);
 
         for(int w:graph.adj(v)){
             if(!visited[w]){
                 dfs(w);
             }
         }
+
+        post.add(v);
     }
 
-    public Iterable<Integer> order(){
-        return order;
+    public Iterable<Integer> pre(){
+        return pre;
+    }
+
+    public Iterable<Integer> post(){
+        return post;
     }
 
     public static void main(String[] args) {
         Graph g = new Graph("GraphDFS/g.txt");
         GraphDFS graphDFS = new GraphDFS(g);
-        System.out.println(graphDFS.order);
+        System.out.println(graphDFS.pre());
+        System.out.println(graphDFS.post());
     }
 }
